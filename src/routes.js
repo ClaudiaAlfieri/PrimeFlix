d4f1bdadad391bdc440f93c84a874dba49f4e5de
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-02-17 11:43:49 +0000

    Create route.js

---
+++ src/routes.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './pages/Home';
+import Filme from './pages/Filme';
+import Erro from './pages/Erro';
+import Favoritos from './pages/Favoritos';
+
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function RoutesApp(){
+    return(
+        <BrowserRouter>
+            <Header/>
+            <main>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/filme/:id" element={<Filme/>}/>
+                 <Route path="/favoritos" element={<Favoritos/>}/>
+
+                <Route path="*" element={<Erro/>}/>
+            </Routes>
+            </main>
+            <Footer/>
+        </BrowserRouter>
+    )
+}
+
+export default RoutesApp;
