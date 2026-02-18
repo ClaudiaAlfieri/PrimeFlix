468793764e7ac5cb75385782ea24d123242086e6
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-02-18 16:22:18 +0000

    Create Footer and css

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import './footer.css';
+import {Link} from 'react-router-dom';
+
+function Footer(){
+    return(
+        <footer>
+            <p>© 2026 Prime Flix</p>          
+        </footer>
+    )
+}
+export default Footer;
